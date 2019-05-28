233f9e77439a4c82ff7065fb6aa348db3e4d06da
Ryan McCormick <ryan.mccormick@rtmccormick.com>
2019-05-27 19:43:58 -0500

    1.0.11

--- lib/models/index.ts
+++ lib/models/index.ts
@@ -4,10 +4,4 @@ import { TimeBlockCalc } from './time-block-calc.model';
 import { TwelveHourParts } from './twelve-hour-parts.model';
 import { TwentyFourHourParts } from './twenty-four-hour-parts.model';
 
-export {
-  NumberRangeItem,
-  TimeBlock,
-  TimeBlockCalc,
-  TwelveHourParts,
-  TwentyFourHourParts,
-};
+export { NumberRangeItem, TimeBlock, TimeBlockCalc, TwelveHourParts, TwentyFourHourParts };
